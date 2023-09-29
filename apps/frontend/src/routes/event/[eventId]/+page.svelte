6e0ce98ca66e40576fba916cdbca8f957bfb2f9e
Armin Ganic <armin.ganic@iteratec.com>
2023-09-29 15:13:11 +0200

    Added title, description and meta fields to event detail page

--- apps/frontend/src/routes/event/[eventId]/+page.svelte
+++ apps/frontend/src/routes/event/[eventId]/+page.svelte
@@ -10,6 +10,18 @@
 
 <div class="page">
 	<img class="page__hero" src="https://picsum.photos/900/500" alt="" />
+	<div class="page__header">
+		<div class="page__header-texts">
+			<h1>{event.title}</h1>
+			<p>{event.desc}</p>
+		</div>
+		<div class="page__meta">
+			<p class="page__meta-info">
+				Starts at: {event.start.day}.{event.start.month}.{event.start.year}
+			</p>
+			<p class="page__meta-info">Ends at: {event.end.day}.{event.end.month}.{event.end.year}</p>
+		</div>
+	</div>
 	<SectionHeader title="Projects">
 		<Button type="primary" href="project/create">Create Project</Button>
 	</SectionHeader>
@@ -24,6 +36,7 @@
 
 <style lang="scss">
 	.page {
+		container-type: inline-size;
 		display: flex;
 		flex-direction: column;
 		gap: 16px;
@@ -31,15 +44,47 @@
 		&__hero {
 			aspect-ratio: 21 / 9;
 			background-color: var(--bg-muted);
-			border: solid 1px var(--line);
+			border: 1px solid var(--line);
 			border-radius: var(--border-radius-medium);
 			object-fit: contain;
+		}
+
+		&__header-texts {
+			display: flex;
+			flex-direction: column;
+			gap: 4px;
+			margin-bottom: 16px;
+		}
+
+		&__meta {
+			border: 1px solid var(--line);
+			border-radius: var(--border-radius-medium);
+			padding-block: 8px;
+
+			&-info {
+				padding: 8px 16px;
+			}
 		}
 
 		&__projects {
 			display: grid;
 			grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 			gap: 16px;
+		}
+
+		@container (width > 700px) {
+			&__header {
+				display: flex;
+				justify-content: space-between;
+
+				&-texts {
+					flex-grow: 2;
+				}
+			}
+
+			&__meta {
+				flex-grow: 1;
+			}
 		}
 	}
 </style>
