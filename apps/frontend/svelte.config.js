453a2a57f26ca328d62819e709bf653e9a7da047
Simon Hayden <simon.hayden@iteratec.com>
2023-09-29 11:43:21 +0200

    add alias to svelte

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,5 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import tsConfigBase from '../../tsconfig.base.json' assert { type: 'json' };
+
+/** @type Record<string, string> */
+const flattened = {};
+Object.entries(tsConfigBase.compilerOptions.paths).forEach(([alias, target]) => {
+	flattened[alias] = target[0];
+});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +18,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		alias: flattened
 	}
 };
 
