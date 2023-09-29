a6d1ca9bb2589a79fc1e440bbf98e166e48e0b22
Lena Schnedlitz <git@lenaschnedlitz.com>
2023-09-29 14:50:06 +0200

    Add field names

--- apps/frontend/src/routes/event/create/+page.svelte
+++ apps/frontend/src/routes/event/create/+page.svelte
@@ -3,25 +3,28 @@
 <form class="form">
 	<label class="form-field form-field--medium">
 		<span>Title<span class="text-muted">*</span></span>
-		<input placeholder="Hackfest" type="text" required /></label
+		<input name="title" placeholder="Hackfest" type="text" required /></label
 	>
 	<label class="form-field"
 		>Summary<textarea
+			name="shortDesc"
 			placeholder="Get ready for the first hackathon of the year!"
 			maxlength="256"
 			rows="3"
 		/></label
 	>
-	<label class="form-field">Description<textarea placeholder="" maxlength="2048" rows="5" /></label>
+	<label class="form-field"
+		>Description<textarea name="desc" placeholder="" maxlength="2048" rows="5" /></label
+	>
 	<fieldset>
 		<legend>Date</legend>
 		<label class="form-field form-field--short">
 			<span>Start Date<span class="text-muted">*</span></span>
-			<input type="date" required /></label
+			<input name="start" type="date" required /></label
 		>
 		<label class="form-field form-field--short">
 			<span>End Date<span class="text-muted">*</span></span>
-			<input type="date" required /></label
+			<input name="end" type="date" required /></label
 		>
 	</fieldset>
 
