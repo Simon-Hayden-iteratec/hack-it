3564bf4efa1ad51d51204c4766614638ad108041
Armin Ganic <armin.ganic@iteratec.com>
2023-09-29 10:26:11 +0200

    Changed class names to BEM

--- apps/frontend/src/lib/components/Navbar.svelte
+++ apps/frontend/src/lib/components/Navbar.svelte
@@ -4,23 +4,23 @@
 </script>
 
 <nav class="nav">
-	<picture class="picture">
+	<picture class="nav__picture">
 		<source srcset={lightLogo} media="(prefers-color-scheme: dark)" />
 		<img src={darkLogo} alt="" />
 	</picture>
 </nav>
 
-<style>
+<style lang="scss">
 	.nav {
 		border-bottom: 1px solid var(--secondary);
 		display: flex;
 		height: 48px;
 		justify-content: center;
 		padding: 16px;
-	}
 
-	.picture {
-		display: inline-flex;
-		height: 100%;
+		&__picture {
+			display: inline-flex;
+			height: 100%;
+		}
 	}
 </style>
