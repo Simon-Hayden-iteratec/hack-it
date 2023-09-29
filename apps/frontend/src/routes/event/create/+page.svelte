649e647ce660fc6db122e9cbe57cb5fd8023e81b
Lena Schnedlitz <git@lenaschnedlitz.com>
2023-09-29 14:29:10 +0200

    Move fieldset styles

--- apps/frontend/src/routes/event/create/+page.svelte
+++ apps/frontend/src/routes/event/create/+page.svelte
@@ -2,7 +2,7 @@
 
 <form class="form">
 	<label class="form-field form-field--medium"
-		>Title<input placeholder="Hackfest" type="text" /></label
+		>Title*<input placeholder="Hackfest" type="text" required /></label
 	>
 	<label class="form-field"
 		>Summary<textarea
@@ -14,10 +14,13 @@
 	<label class="form-field">Description<textarea placeholder="" maxlength="2048" rows="5" /></label>
 	<fieldset>
 		<legend>Date</legend>
-		<label class="form-field form-field--short">Start Date<input type="date" /></label>
-		<label class="form-field form-field--short">End Date<input type="date" /></label>
+		<label class="form-field form-field--short">Start Date*<input type="date" required /></label>
+		<label class="form-field form-field--short">End Date*<input type="date" required /></label>
 	</fieldset>
 </form>
 
 <style>
+	fieldset > label {
+		flex: 1 1 50%;
+	}
 </style>
