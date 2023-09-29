631b90632677d6ae401b217909c05f23b3f22165
Lena Schnedlitz <git@lenaschnedlitz.com>
2023-09-29 14:18:36 +0200

    Add draft event form

--- apps/frontend/src/routes/event/create/+page.svelte
+++ apps/frontend/src/routes/event/create/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Create[Event]</h1>
+<h1>Create Event</h1>
+
+<form class="form">
+	<label class="form-field form-field--medium"
+		>Title<input placeholder="Hackfest" type="text" /></label
+	>
+	<label class="form-field"
+		>Summary<textarea
+			placeholder="Get ready for the first hackathon of the year!"
+			maxlength="256"
+			rows="3"
+		/></label
+	>
+	<label class="form-field">Description<textarea placeholder="" maxlength="2048" rows="5" /></label>
+	<fieldset>
+		<legend>Date</legend>
+		<label class="form-field form-field--short">Start Date<input type="date" /></label>
+		<label class="form-field form-field--short">End Date<input type="date" /></label>
+	</fieldset>
+</form>
+
+<style>
+</style>
